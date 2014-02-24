c509ee9f43511eb41042562d1f4cf2977e0f0594
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-24 16:27:13 +1100

    Added example

---
+++ examples/file-transfer.js
@@ -0,0 +1,45 @@
+var dropkick = require('dropkick');
+var quickconnect = require('rtc-quickconnect');
+var fileReader = require('filestream/read');
+var fileReceiver = require('filestream/write');
+var createDataStream = require('..');
+var channels = [];
+var peers = [];
+var inbound = {};
+
+function prepStream(dc, id) {
+  createDataStream(dc).pipe(fileReceiver(function(file, metadata) {
+    console.log('received file from: ' + id, file, metadata);
+
+    // get ready to receive another stream
+    setTimeout(function() {
+      prepStream(dc, id);
+    }, 50);
+  }));
+}
+
+quickconnect('http://rtc.io/switchboard', { room: 'filetx-test' })
+  .createDataChannel('files')
+  .on('files:open', function(dc, id) {
+    channels.push(dc);
+    peers.push(id);
+
+    prepStream(dc, id);
+  })
+  .on('peer:leave', function(id) {
+    var peerIdx = peers.indexOf(id);
+    if (peerIdx >= 0) {
+      peers.splice(peerIdx, 1);
+      channels.splice(peerIdx, 1);
+    }
+  })
+
+dropkick(document.body).on('file', function(file) {
+  channels.map(createDataStream).forEach(function(stream) {
+    fileLoader(file).pipe(stream);
+  });
+});
+
+// give the document some size so we can drag and drop stuff
+document.body.style.width = '100vw';
+document.body.style.height = '100vw';
